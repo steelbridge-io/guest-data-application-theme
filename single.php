a3fde2387edb9e10a4fb80644b16610f16ed2b85
steelbridge-io <chris@steelbridge.io>
2024-11-08 16:33:11 -0800

    Change template path in single.php
    Corrected the path for the content-single template part to improve file inclusion accuracy. This ensures the template is properly loaded, addressing potential rendering issues in single posts.

--- single.php
+++ single.php
@@ -2,7 +2,7 @@
 get_header(); ?>
   <div class="wrapper">
     <div class="container single-post mt-5 mb-5">
-      <?php get_template_part('template-parts/content-single.php', 'single'); ?>
+      <?php get_template_part('/template-parts/content', 'single'); ?>
     </div>
     <?php get_footer(); ?>
   </div>
