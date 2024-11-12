5960d2850545f6f9ab818d2eda61ef7c02f1f836
steelbridge-io <chris@steelbridge.io>
2024-11-11 20:09:00 -0800

    Enhance login and registration forms layout
    Reorganized the HTML structure for the login and registration forms by adding Bootstrap's grid system and card components. Updated the CSS to improve form styling, including the removal of the flex display from the .form-container and adding customized margins and padding for better spacing and alignment. These changes enhance the visual appeal and responsiveness of the forms.

--- index.php
+++ index.php
@@ -7,49 +7,58 @@ get_header(); ?>
 
         <?php //if (!is_user_logged_in()): // Check if user is not logged in ?>
           <div class="form-container">
-            <div class="login-form">
-              <h2>Login</h2>
-              <form action="<?php echo wp_login_url(); ?>" method="post">
-                <p>
-                  <label for="log">Username or Email<br />
-                    <input type="text" name="log" id="log" class="input" value="" size="20" /></label>
-                </p>
-                <p>
-                  <label for="pwd">Password<br />
-                    <input type="password" name="pwd" id="pwd" class="input" value="" size="20" /></label>
-                </p>
-                <p>
-                  <label for="rememberme">
-                    <input type="checkbox" name="rememberme" id="rememberme" value="forever" /> Remember Me</label>
-                </p>
-                <p>
-                  <input type="submit" name="wp-submit" id="wp-submit" value="Log In" />
-                </p>
-              </form>
+            <div class="row">
+              <div class="col-md-6">
+                <div class="card">
+                <div class="login-form">
+                  <h2>Login</h2>
+                  <form action="<?php echo wp_login_url(); ?>" method="post">
+                    <p>
+                      <label for="log">Username or Email<br />
+                        <input type="text" name="log" id="log" class="input" value="" size="20" /></label>
+                    </p>
+                    <p>
+                      <label for="pwd">Password<br />
+                        <input type="password" name="pwd" id="pwd" class="input" value="" size="20" /></label>
+                    </p>
+                    <p>
+                      <label for="rememberme">
+                        <input type="checkbox" name="rememberme" id="rememberme" value="forever" /> Remember Me</label>
+                    </p>
+                    <p>
+                      <input type="submit" name="wp-submit" id="wp-submit" value="Log In" />
+                    </p>
+                  </form>
+                </div>
+              </div>
             </div>
-
-            <div class="registration-form">
-              <h2>Register</h2>
-              <form action="<?php echo esc_url(admin_url('admin-post.php')); ?>" method="post">
-                <input type="hidden" name="action" value="register_user">
-                <p>
-                  <label for="user_login">Username<br />
-                    <input type="text" name="user_login" id="user_login" class="input" value="" size="20" /></label>
-                </p>
-                <p>
-                  <label for="user_email">Email<br />
-                    <input type="email" name="user_email" id="user_email" class="input" value="" size="25" /></label>
-                </p>
-                <p>
-                  <label for="user_password">Password<br />
-                    <input type="password" name="user_password" id="user_password" class="input" value="" size="25" /></label>
-                </p>
-                <p>
-                  <input type="submit" name="wp-submit" id="wp-submit" value="Register" />
-                </p>
-              </form>
+            <div class="col-md-6">
+              <div class="card">
+              <div class="registration-form">
+                <h2>Register</h2>
+                <form action="<?php echo esc_url(admin_url('admin-post.php')); ?>" method="post">
+                  <input type="hidden" name="action" value="register_user">
+                  <p>
+                    <label for="user_login">Username<br />
+                      <input type="text" name="user_login" id="user_login" class="input" value="" size="20" /></label>
+                  </p>
+                  <p>
+                    <label for="user_email">Email<br />
+                      <input type="email" name="user_email" id="user_email" class="input" value="" size="25" /></label>
+                  </p>
+                  <p>
+                    <label for="user_password">Password<br />
+                      <input type="password" name="user_password" id="user_password" class="input" value="" size="25" /></label>
+                  </p>
+                  <p>
+                    <input type="submit" name="wp-submit" id="wp-submit" value="Register" />
+                  </p>
+                </form>
+              </div>
+            </div>
             </div>
           </div>
+        </div>
         <?php //else: // If user is logged in ?>
           <!-- <p>You are already logged in.</p> -->
         <?php //endif; ?>
