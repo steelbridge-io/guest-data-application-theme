7506a9d4fe787bfbd968e27bb749dca98d4ea1a7
steelbridge-io <chris@steelbridge.io>
2024-11-20 20:06:43 -0800

    Update styles and templates for guest data application
    Refined several CSS styles for better layout consistency and readability. Improved structure and styling in PHP templates to enhance user experience and maintainability.

--- page-templates/travel-gda-table.php
+++ page-templates/travel-gda-table.php
@@ -6,8 +6,9 @@
 
 get_header(); ?>
 
-<div class="container container-flex-none mt-5">
-  <h1><?php echo the_title(); ?></h1>
+<div class="container-fluid guest-data-list-wrapper">
+<div class="container container-flex-none">
+<h1><?php echo the_title(); ?></h1>
 
 <?php
 if (have_posts()) :
@@ -26,46 +27,47 @@ endif;
 ?>
 </div>
 
-  <div class="container travel-item-list mt-5 mb-5">
-    <div class="row">
-      <div class="col-12">
-        <div class="well mt-5 mb-5">
-          <?php echo do_shortcode('[custom_search post_type="travel-form"]'); ?>
-        </div>
+<div class="container travel-item-list padding-top-1 padding-bottom-2">
+  <div class="row">
+    <div class="col-12">
+      <div class="well mt-5 mb-5">
+        <?php echo do_shortcode('[custom_search post_type="travel-form"]'); ?>
       </div>
     </div>
-    <div class="row">
-      <?php
-      // Query for the custom post type 'travel-form'
-      $args = array(
-        'post_type' => 'travel-form',
-        'posts_per_page' => -1, // Get all posts
-      );
-
-      $query = new WP_Query($args);
-
-      if ($query->have_posts()) :
-        $count = 0;
-        while ($query->have_posts()) : $query->the_post();
-          // Start a new row every 4 posts
-          // if ($count > 0 && $count % 4 == 0) {
-          //echo '</div><div class="row">';
-          // }
-          ?>
-          <div class="col-md-6 travel-item">
-            <div class="card">
-              <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-            </div>
-          </div>
-          <?php
-          $count++;
-        endwhile;
-        wp_reset_postdata();
-      else :
-        echo '<p>No travel questionnaires found.</p>';
-      endif;
-      ?>
-    </div>
   </div>
+  <div class="row">
+    <?php
+    // Query for the custom post type 'travel-form'
+    $args = array(
+      'post_type' => 'travel-form',
+      'posts_per_page' => -1, // Get all posts
+    );
+
+    $query = new WP_Query($args);
+
+    if ($query->have_posts()) :
+      $count = 0;
+      while ($query->have_posts()) : $query->the_post();
+        // Start a new row every 4 posts
+        // if ($count > 0 && $count % 4 == 0) {
+        //echo '</div><div class="row">';
+        // }
+        ?>
+        <div class="col-md-6 travel-item">
+          <div class="card">
+            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+          </div>
+        </div>
+        <?php
+        $count++;
+      endwhile;
+      wp_reset_postdata();
+    else :
+      echo '<p>No travel questionnaires found.</p>';
+    endif;
+    ?>
+  </div>
+ </div>
+</div>
 
 <?php get_footer(); ?>
