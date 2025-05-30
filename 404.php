599aede4bc1abea7af3a3427116cb486c1451892
steelbridge-io <chris@steelbridge.io>
2025-05-29 21:35:35 -0700

    Update 404 page content with improved messaging for private content and login guidance
    Enhanced the 404.php template by refining user messaging to clarify login requirements for private content. Added a call-to-action for logging in alongside improved styling and formatting for better user experience.

--- 404.php
+++ 404.php
@@ -3,13 +3,16 @@ get_header();
 ?>
 
   <main id="main-content">
-    <article id="post-404" class="post-not-found">
-      <header class="entry-header">
-        <h1><?php esc_html_e('Page Not Found', 'guest-data-application-theme'); ?></h1>
+    <article id="post-404" class="post-not-found container">
+      <header class="entry-header mt-5">
+        <h1><?php esc_html_e('Whoops!', 'guest-data-application-theme'); ?></h1>
       </header>
       <div class="entry-content">
-        <p><?php esc_html_e('Sorry, but the page you were trying to view does not exist.', 'guest-data-application-theme'); ?></p>
-        <p><?php esc_html_e('It looks like this was the result of either a mistyped address or an out-of-date link.', 'guest-data-application-theme'); ?></p>
+        <p class="lead"><?php esc_html_e('It is possible you are seeing this message becuase you are not logged-in?',
+					'guest-data-application-theme'); ?></p>
+        <p class="lead"><?php esc_html_e('Some of the content on this website is private and only a logged-in user can veiw certain types of content. If logging in does not address the problem you are experiencing, please send us a message at info@theflyshop.com and we will help you resolve this issue.',	'guest-data-application-theme'); ?></p>
+			 <p class="lead"><?php esc_html_e('Log in here:&nbsp;', 'guest-data-application-theme'); ?><a href="https://www.theflyshop.com/gda/" title="Log in"><b>Log In</b></a></p>
+
       </div>
     </article>
   </main>
